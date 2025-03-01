66621cc5c4c8dc2f15b6447344360dac6c3cc610
Ygnacio Martínez Sánchez <alu.20130778@correo.itlalaguna.edu.mx>
2025-02-28 21:53:44 -0600

    Update Router basename in App component to reflect new distribution path

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ProductDetail from './views/ProductDetail.jsx';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/mico/dist">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetail />} />
